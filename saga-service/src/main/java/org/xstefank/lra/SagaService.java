ed0cbee17dab4ef78aa0334ef7dc84dc4569aab0
xstefank <xstefank122@gmail.com>
2018-03-27 21:58:57 +0200

    Adjust participant2 for the new API

--- saga-service/src/main/java/org/xstefank/lra/SagaService.java
+++ saga-service/src/main/java/org/xstefank/lra/SagaService.java
@@ -7,7 +7,9 @@ import org.xstefank.lra.definition.rest.RESTLraBuilder;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.core.UriBuilder;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 @Stateless
@@ -17,12 +19,18 @@ public class SagaService {
     @CurrentLRAClient
     private NarayanaLRAClient lraClient;
 
-    public String startSaga(String name, String uri) throws MalformedURLException {
+    public String startSaga(String name, String uri) throws MalformedURLException, URISyntaxException {
+
+        URL participant2Url = new URL("http://participant2-service:8080/api");
+
         RESTLra lra = RESTLraBuilder.lra()
                 .name("testing saga")
                 .data(42)
                 .withAction(RESTAction.post(new URL("http://participant1-service:8080/api")).build())
-//                .withAction(RESTAction.post(new URL("http://participant2-service:8080/api/request")))
+                .withAction(RESTAction
+                        .post(UriBuilder.fromUri(participant2Url.toURI()).path("/request").build().toURL())
+                        .callbackUrl(participant2Url)
+                        .build())
                 .callback(uri)
                 .build();
 
